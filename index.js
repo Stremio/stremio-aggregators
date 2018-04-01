6f3e13ab8a939607ef0caf246fbd85226cc95bb6
Ivo Georgiev <ivo@linvo.org>
2018-04-02 00:35:34 +0300

    streams aggregator added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const addonStore = require('./testAddons')
 
 const Generic = require('./lib/generic')
 const Catalogs = require('./lib/catalogs')
+const Streams = require('./lib/streams')
 
 // TEMP
 const aggr = Catalogs(addonStore.addons)
@@ -12,8 +13,26 @@ const aggr = Catalogs(addonStore.addons)
 aggr.on('finished', function() {
 	aggr.results.forEach(function(r) {
 		console.log(r.addon.manifest.id, r.type, r.id, r.response.metas.slice(0, 5).map(function(x) { return x.name }))
+		
+		// @NOTE: has to be documented; r.response is CERTAINLY here but not r.response.metas
+
+		// TEMP
+		if (r.addon.manifest.id === 'com.linvo.cinemeta' && r.type === 'series' && r.response.metas.length) 
+			tryStreamsSeries(r.response.metas[0])
 	})
 })
+
+function tryStreamsSeries(meta) {
+	const aggr = Streams(addonStore.addons, meta.type, meta.imdb_id+':1:1')
+
+	aggr.on('finished', function() {
+		// @NOTE: has to be documented; r.response is CERTAINLY here but not r.response.streams
+
+		aggr.results.forEach(function(r) {
+			console.log(r.response.streams)
+		})
+	})
+}
 // TEMP
 
 module.exports = {
