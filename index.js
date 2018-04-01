309594b6a74eef77f156881ab01cecd1ac571922
Ivo Georgiev <ivo@linvo.org>
2018-04-02 00:03:54 +0300

    Trying to figure out addon collection

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const detectFromURL = require('stremio-addon-client').detectFromURL
 
-const addonURLs = require('./testAddons')
+const addonStore = require('./testAddons')
 // require('stremio-addon-client/lib/transports')
 // to persist addons
 
@@ -8,17 +8,19 @@ const Generic = require('./lib/generic')
 const Catalogs = require('./lib/catalogs')
 
 // TEMP
-Promise.all(addonURLs.map(function(x) { return detectFromURL(x) }))
+
+/*Promise.all(addonURLs.map(function(x) { return detectFromURL(x) }))
 .then(function(results) {
 	const addons = results
 		.map(function(x) { return x.addon })
 		.filter(function(x) { return x })
 
-	const aggr = Catalogs(addons)
 
-	aggr.on('finished', function() {
-		console.log(aggr.results)
-	})
+})*/
+const aggr = Catalogs(addonStore.addons)
+
+aggr.on('finished', function() {
+	console.log(aggr.results)
 })
 // TEMP
 
