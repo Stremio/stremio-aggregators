8654199b159ab9481a25fc650204c8f4764bd20a
Ivo Georgiev <ivo@linvo.org>
2018-04-09 20:16:12 +0300

    basic meta aggregator

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const Generic = require('./lib/generic')
 const Catalogs = require('./lib/catalogs')
 const Streams = require('./lib/streams')
-
+const Metas = require('./lib/metas')
 
 module.exports = {
 	Generic: Generic,
 	Catalogs: Catalogs,
 	Streams: Streams,
+	Metas: Metas,
 }
