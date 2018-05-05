ce238324ec529c1059f7273b202f0454aae64976
Ivo Georgiev <ivo@strem.io>
2018-05-05 15:57:48 +0100

    every result now gets an assigned uid

--- test/generic.js
+++ test/generic.js
@@ -8,10 +8,11 @@ const Generic = require('../lib/generic')
 tape('generic: emits proper events', function(t) {
 	let aggr = new Generic()
 
-	aggr.results = [
-		{ isReady: true, response: null },
-		{ isReady: true, response: null }
-	]
+	aggr.pushResult({ isReady: true, response: null })
+	aggr.pushResult({ isReady: true, response: null })
+
+	t.equal(aggr.results[0].uid, 0, 'uid for results[0] is valid')
+	t.equal(aggr.results[1].uid, 1, 'uid for results[1] is valid')
 
 	// note that .run() is executed first, and we bind to events later
 	// we expect that events will be emitted at least one tick after .run()
