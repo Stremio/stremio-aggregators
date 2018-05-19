d5e3b15dc51f81ffa1a2cf123eec92cc8eee4193
Ivo Georgiev <ivo@strem.io>
2018-05-19 14:57:21 +0200

    add getStreamsCached

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const Catalogs = require('./lib/catalogs')
 const Streams = require('./lib/streams')
 const Metas = require('./lib/metas')
 
+const getStreamsCached = require('./lib/getStreamsCached')
+
 module.exports = {
 	Generic: Generic,
 	Catalogs: Catalogs,
 	Streams: Streams,
 	Metas: Metas,
+
+	getStreamsCached: getStreamsCached,
 }
