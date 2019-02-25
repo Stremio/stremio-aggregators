4b811e96ecb8e44a71d73b842ea2520a03e9ee7c
Ivo Georgiev <ivo@strem.io>
2019-02-25 10:44:07 +0000

    isCatalogSupported supports new extra notation, tested

--- test/isCatalogSupported.js
+++ test/isCatalogSupported.js
@@ -8,7 +8,7 @@ tape('isCatalogSupported: simple catalog', function(t) {
 })
 
 
-tape('isCatalogSupported: extraSupported', function(t) {
+function testExtraSupported(t, genCatalog) {
 	// all properties in extra should be in catalog.extraSupported
 	t.equal(isCatalogSupported(genCatalog(['search']), null), true, 'no extra: should match')
 	
@@ -19,9 +19,9 @@ tape('isCatalogSupported: extraSupported', function(t) {
 	t.equal(isCatalogSupported(genCatalog(['search']), { genre: 'foo' }), false, 'extra does not match')
 
 	t.end()
-})
+}
 
-tape('isCatalogSupported: extraRequired', function(t) {
+function testExtraRequired(t, genCatalog) {
 	// all properties in catalog.extraRequired should be in extra
 	t.equal(isCatalogSupported(genCatalog(['search'], ['search']), null), false, 'no extra: should not match')
 	t.equal(isCatalogSupported(genCatalog(['search'], ['search']), { genre: 'foo' }), false, 'extra does not contain search: should not match')
@@ -32,9 +32,29 @@ tape('isCatalogSupported: extraRequired', function(t) {
 	t.equal(isCatalogSupported(genCatalog(['search', 'genre'], ['search']), { search: 'foo', genre: 'foo' }), true, 'does support genre')
 
 	t.end()
-})
+}
+
+// Test short notation
+tape('isCatalogSupported: extraSupported', function(t) { testExtraSupported(t, genCatalog) })
+tape('isCatalogSupported: extraRequired', function(t) { testExtraRequired(t, genCatalog) })
+
+// Test full notation
+tape('isCatalogSupported: extra (full notation): supported', function(t) { testExtraSupported(t, genCatalogFullNotation) })
+tape('isCatalogSupported: extra (full notation): required', function(t) { testExtraRequired(t, genCatalogFullNotation) })
 
 // small helper to stop repeating ourselves
 function genCatalog(supported, required) {
 	return { type: 'movie', id: 'top', extraSupported: supported, extraRequired: required }
+}
+function genCatalogFullNotation(supported, required) {
+	return {
+		type: 'movie',
+		id: 'top',
+		extra: supported.map(function(name) {
+			return {
+				name: name,
+				isRequired: required ? required.includes(name) : false,
+			}
+		})
+	}
 }
